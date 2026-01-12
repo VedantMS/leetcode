7cd1384e89a1cc8ce0bd9443a4eb15d3ef242cc6
Vedant Manoj Salunke <vedantsalunke1234@gmail.com>
2026-01-12 20:03:49 +0530

    Time: 1 ms (95.28%) | Memory: 45 MB (15.50%) - LeetSync

--- 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.java
+++ 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.java
@@ -3,9 +3,9 @@ class Solution {
         
         int ans = 0;
         for(int i = 0; i < points.length - 1; i++) {
-            int rows = Math.abs(points[i][0] - points[i + 1][0]);   // Rows to Traverse
-            int cols = Math.abs(points[i][1] - points[i + 1][1]);   // Columns to Traverse
-            ans = ans + Math.max(rows, cols);
+            int dx = Math.abs(points[i][0] - points[i + 1][0]);   // Units to Traverse Horizontally
+            int dy = Math.abs(points[i][1] - points[i + 1][1]);   // Units to Traverse Vertically
+            ans += Math.max(dx, dy);
         }
         return ans;
     }
