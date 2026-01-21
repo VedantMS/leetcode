60c03d2fc18a65387317f0c12287e7dedf83aabf
Vedant Manoj Salunke <vedantsalunke1234@gmail.com>
2026-01-22 00:18:17 +0530

    Time: 6 ms (52.38%) | Memory: 18.1 MB (33.33%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.c
+++ 1073-number-of-enclaves/number-of-enclaves.c
@@ -1,15 +1,8 @@
-int flag;
-int count;
 void dfs(int **grid, int rows, int cols, int r, int c) {
-    if(r < 0 || r >= rows || c < 0 || c >= cols) {
-        flag = 1;
-        return;
-    }
-    else if(grid[r][c] == 0) {
+    if(r < 0 || r >= rows || c < 0 || c >= cols || grid[r][c] == 0) {
         return;
     }
     grid[r][c] = 0;
-    count++;
     dfs(grid, rows, cols, r, c - 1);
     dfs(grid, rows, cols, r, c + 1);
     dfs(grid, rows, cols, r - 1, c);
@@ -19,15 +12,23 @@ int numEnclaves(int** grid, int gridSize, int* gridColSize) {
     int rows = gridSize;
     int cols = *gridColSize;
     int ans = 0;
-    for(int i = 0; i < rows; i++) {
-        for(int j = 0; j < cols; j++) {
-            if(grid[i][j] == 1) {
-                flag = 0;
-                count = 0;
-                dfs(grid, rows, cols, i, j);
-                if(!flag) {
-                    ans += count;
+    int i = 0, j = 0;
+    while(i < rows) {
+        j = 0;
+        while(j < cols) {
+            if(i == 0 || j == 0 || i == rows - 1 || j == cols - 1) {
+                if(grid[i][j] == 1) {
+                    dfs(grid, rows, cols, i, j);
                 }
+            }
+            j++;
+        }
+        i++;
+    }
+    for(i = 1; i < rows - 1; i++) {
+        for(j = 1; j < cols - 1; j++) {
+            if(grid[i][j] == 1) {
+                ans++;
             }
         }
     }
