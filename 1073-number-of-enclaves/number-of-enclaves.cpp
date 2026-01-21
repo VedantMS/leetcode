fa1a60fc06ec11a557c6de64c42070520a131d9c
Vedant Manoj Salunke <vedantsalunke1234@gmail.com>
2026-01-22 00:12:15 +0530

    Time: 0 ms (100.00%) | Memory: 34.2 MB (51.72%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.cpp
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -1,19 +1,12 @@
 class Solution {
 public:
-    int flag;
-    int count;
     void dfs(vector<vector<int>> &grid, int r, int c) {
         int rows = grid.size();
         int cols = grid[0].size();
-        if(r < 0 || r >= rows || c < 0 || c >= cols) {
-            flag = 1;
-            return;
-        }
-        else if(grid[r][c] == 0) {
+        if(r < 0 || r >= rows || c < 0 || c >= cols || grid[r][c] == 0) {
             return;
         }
         grid[r][c] = 0;
-        count++;
         dfs(grid, r, c - 1);
         dfs(grid, r, c + 1);
         dfs(grid, r - 1, c);
@@ -23,15 +16,23 @@ public:
         int rows = grid.size();
         int cols = grid[0].size();
         int ans = 0;
-        for(int i = 0; i < rows; i++) {
-            for(int j = 0; j < cols; j++) {
-                if(grid[i][j] == 1) {
-                    flag = 0;
-                    count = 0;
-                    dfs(grid, i, j);
-                    if(!flag) {
-                        ans += count;
+        int i = 0, j = 0;
+        while(i < rows) {
+            j = 0;
+            while(j < cols) {
+                if(i == 0 || j == 0 || i == rows - 1 || j == cols - 1) {
+                    if(grid[i][j] == 1) {
+                        dfs(grid, i, j);
                     }
+                }
+                j++;
+            }
+            i++;
+        }
+        for(i = 1; i < rows - 1; i++) {
+            for(j = 1; j < cols - 1; j++) {
+                if(grid[i][j] == 1) {
+                    ans++;
                 }
             }
         }
