45711b0752a3732eff03ebbac9d99eca1d248010
Vedant Manoj Salunke <vedantsalunke1234@gmail.com>
2026-01-23 22:38:28 +0530

    Time: 16 ms (26.02%) | Memory: 26.7 MB (21.08%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     vector<vector<int>> result;
+    vector<vector<bool>> pacific;
+    vector<vector<bool>> atlantic;
+
     void dfs(vector<vector<int>> &heights, vector<vector<bool>> &visited, int r, int c) {
         int rows = heights.size();
         int cols = heights[0].size();
@@ -9,6 +12,8 @@ public:
         
         visited[r][c] = true;
         
+        if(pacific[r][c] && atlantic[r][c])     result.push_back({r, c});
+
         if(c - 1 >= 0 && heights[r][c] <= heights[r][c - 1])        dfs(heights, visited, r, c - 1);    // Left
         if(c + 1 < cols && heights[r][c] <= heights[r][c + 1])      dfs(heights, visited, r, c + 1);    // Right
         if(r - 1 >= 0 && heights[r][c] <= heights[r - 1][c])        dfs(heights, visited, r - 1, c);    // Up
@@ -19,19 +24,11 @@ public:
         int rows = heights.size();
         int cols = heights[0].size();
 
-        vector<vector<bool>> pacific(rows, vector<bool>(cols, false));
-        vector<vector<bool>> atlantic(rows, vector<bool>(cols, false));
+        pacific.assign(rows, vector<bool>(cols, false));
+        atlantic.assign(rows, vector<bool>(cols, false));
         
         for(int i = 0; i < cols; i++)   dfs(heights, pacific, 0, i), dfs(heights, atlantic, rows - 1, i);
         for(int i = 0; i < rows; i++)   dfs(heights, pacific, i, 0), dfs(heights, atlantic, i, cols - 1);
-
-        for(int i = 0; i < rows; i++) {
-            for(int j = 0; j < cols; j++) {
-                if(pacific[i][j] && atlantic[i][j]) {
-                    result.push_back({i, j});
-                }
-            }
-        }
 
         return result;
     }
