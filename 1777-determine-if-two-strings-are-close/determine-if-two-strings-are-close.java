b65e15bd3b74f48f9377033834b5a77be7a162c7
Vedant Manoj Salunke <vedantsalunke1234@gmail.com>
2026-01-24 15:59:38 +0530

    Time: 11 ms (85.09%) | Memory: 48 MB (63.30%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.java
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.java
@@ -1,26 +1,22 @@
 class Solution {
     public boolean closeStrings(String word1, String word2) {
-        TreeMap<Character, Integer> t1 = new TreeMap<>();
-        TreeMap<Character, Integer> t2 = new TreeMap<>();
-        
-        int len1 = word1.length();
-        int len2 = word2.length();
-        
-        if(len1 != len2)    return false;
+        if(word1.length() != word2.length())    return false;
 
-        for(int i = 0; i < len1; i++) {
-            t1.put(word1.charAt(i), t1.getOrDefault(word1.charAt(i), 0) + 1);
-            t2.put(word2.charAt(i), t2.getOrDefault(word2.charAt(i), 0) + 1);
+        int[] freq1 = new int[26];
+        int[] freq2 = new int[26];
+        
+        for(char ch : word1.toCharArray())    freq1[ch - 'a']++;
+        for(char ch : word2.toCharArray())    freq2[ch - 'a']++;
+
+        for(int i = 0; i < 26; i++) {
+            if((freq1[i] == 0) != (freq2[i] == 0)) {
+                return false;
+            }
         }
 
-        if(!t1.keySet().equals(t2.keySet()))    return false;
+        Arrays.sort(freq1);
+        Arrays.sort(freq2);
 
-        ArrayList<Integer> l1 = new ArrayList<>(t1.values());
-        ArrayList<Integer> l2 = new ArrayList<>(t2.values());
-
-        Collections.sort(l1);
-        Collections.sort(l2);
-        
-        return l1.equals(l2);
+        return Arrays.equals(freq1, freq2);
     }
 }
