d4e473490b7bb2d3b3dc23e8e2ce00dce0af8415
Vedant Manoj Salunke <vedantsalunke1234@gmail.com>
2026-01-28 17:01:48 +0530

    Time: 11 ms (84.78%) | Memory: 34.6 MB (92.45%) - LeetSync

---
+++ 542-01-matrix/01-matrix.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int rows = mat.size();
+        int cols = mat[0].size();
+        vector<vector<int>> result(rows, vector<int>(cols, -1));
+
+        int dr[] = {-1, 1, 0, 0};
+        int dc[] = {0, 0, -1, 1};
+
+        queue<pair<int, int>> q;
+
+        if(rows == 1 && cols == 1) {
+            result[0][0] = 0;
+            return result;
+        }
+
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < cols; j++) {
+                if(mat[i][j] == 0) {
+                    result[i][j] = 0;
+                    q.push({i, j});
+                }
+            }
+        }
+
+        while(!q.empty()) {
+            int level = q.size();
+
+            for(int i = 0; i < level; i++) {
+                int r = q.front().first;
+                int c = q.front().second;
+                q.pop();
+
+                for(int j = 0; j < 4; j++) {
+                    int nr = r + dr[j];
+                    int nc = c + dc[j];
+
+                    if(nr < rows && nr >= 0 && nc < cols && nc >= 0 && result[nr][nc] == -1) {
+                        result[nr][nc] = result[r][c] + 1;
+                        q.push({nr, nc});
+                    }
+                }
+            }
+        }
+        return result;
+    }
+};
