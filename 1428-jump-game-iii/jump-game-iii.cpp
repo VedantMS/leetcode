6649131878116fe2baffd71e4417f99fbba34768
Vedant Manoj Salunke <vedantsalunke1234@gmail.com>
2026-01-29 10:55:20 +0530

    Time: 0 ms (100.00%) | Memory: 36.5 MB (63.04%) - LeetSync

---
+++ 1428-jump-game-iii/jump-game-iii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool canReach(vector<int>& arr, int start) {
+        int n = arr.size();
+        queue<int> q;
+        vector<int> visited(n, false);
+        
+        q.push(start);
+    
+        while(!q.empty()) {
+            int level = q.size();
+            for(int i = 0; i < level; i++) {
+                int pos = q.front();
+                q.pop();
+
+                if(pos + arr[pos] < n && !visited[pos + arr[pos]]) {
+                    if(arr[pos + arr[pos]] == 0) {
+                        return true;
+                    }
+                    q.push(pos + arr[pos]);
+                    visited[pos + arr[pos]] = true;
+                }
+                
+                if(pos - arr[pos] >= 0 && !visited[pos - arr[pos]]) {
+                    if(arr[pos - arr[pos]] == 0) {
+                        return true;
+                    }
+                    q.push(pos - arr[pos]);
+                    visited[pos - arr[pos]] = true;
+                }
+            }
+        }
+        return false;
+    }
+};
