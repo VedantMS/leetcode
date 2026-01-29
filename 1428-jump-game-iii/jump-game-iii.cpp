d1d4f136b7514fa669b3f8d20432c278293101b4
Vedant Manoj Salunke <vedantsalunke1234@gmail.com>
2026-01-29 11:05:49 +0530

    Time: 0 ms (100.00%) | Memory: 36.6 MB (63.04%) - LeetSync

--- 1428-jump-game-iii/jump-game-iii.cpp
+++ 1428-jump-game-iii/jump-game-iii.cpp
@@ -6,23 +6,22 @@ public:
         vector<int> visited(n, false);
         
         q.push(start);
+        visited[start] = true;
     
         while(!q.empty()) {
             int pos = q.front();
             q.pop();
 
+            if(arr[pos] == 0) {
+                return true;
+            }
+
             if(pos + arr[pos] < n && !visited[pos + arr[pos]]) {
-                if(arr[pos + arr[pos]] == 0) {
-                    return true;
-                }
                 q.push(pos + arr[pos]);
                 visited[pos + arr[pos]] = true;
             }
 
             if(pos - arr[pos] >= 0 && !visited[pos - arr[pos]]) {
-                if(arr[pos - arr[pos]] == 0) {
-                    return true;
-                }
                 q.push(pos - arr[pos]);
                 visited[pos - arr[pos]] = true;
             }
