7a3998d81135cc78a0c87a179d1e550c1bc00106
Vedant Manoj Salunke <vedantsalunke1234@gmail.com>
2026-01-31 14:12:13 +0530

    Time: 1 ms (31.25%) | Memory: 15.9 MB (51.25%) - LeetSync

---
+++ 130-surrounded-regions/surrounded-regions.c
@@ -0,0 +1,52 @@
+void dfs(char** board, int rows, int cols, int r, int c) {
+    board[r][c] = 'A';
+
+    int dr[] = {-1, 1, 0, 0};
+    int dc[] = {0, 0, -1, 1};
+
+    for(int i = 0; i < 4; i++) {
+        int nr = r + dr[i];
+        int nc = c + dc[i];
+        if(nr < rows && nr >= 0 && nc < cols && nc >= 0 && board[nr][nc] == 'O') {
+            dfs(board, rows, cols, nr, nc);
+        }
+    }
+}
+
+
+void solve(char** board, int boardSize, int* boardColSize) {
+    int rows = boardSize;
+    int cols = *boardColSize;
+
+    int i = 0;
+    int j;
+
+    while(i < rows) {
+        j = 0;
+        while(j < cols) {
+            if(i == 0 || i == rows - 1 || j == 0 || j == cols - 1) {
+                if(board[i][j] == 'O') {
+                    dfs(board, rows, cols, i, j);
+                }
+            }
+            j++;
+        }
+        i++;
+    }
+
+    for(i = 1; i < rows - 1; i++) {
+        for(j = 1; j < cols - 1; j++) {
+            if(board[i][j] == 'O') {
+                board[i][j] = 'X';
+            }
+        }
+    }
+
+    for(i = 0; i < rows; i++) {
+        for(j = 0; j < cols; j++) {
+            if(board[i][j] == 'A') {
+                board[i][j] = 'O';
+            }
+        }
+    }
+}
