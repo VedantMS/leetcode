85df80720fe324acad6f2cc76ecf7bf9d7acce71
Vedant Manoj Salunke <vedantsalunke1234@gmail.com>
2026-02-04 19:21:10 +0530

    Time: 4 ms (30.77%) | Memory: 43.7 MB (22.12%) - LeetSync

--- 1661-minimum-number-of-vertices-to-reach-all-nodes/minimum-number-of-vertices-to-reach-all-nodes.c
+++ 1661-minimum-number-of-vertices-to-reach-all-nodes/minimum-number-of-vertices-to-reach-all-nodes.c
@@ -3,12 +3,15 @@
  */
 int* findSmallestSetOfVertices(int n, int** edges, int edgesSize, int* edgesColSize, int* returnSize) {
     int *indegree = (int *)calloc(n, sizeof(int));
-    int count = n;
     for(int i = 0; i < edgesSize; i++) {
-        if(indegree[edges[i][1]] == 0) {
-            count--;
-        }
         indegree[edges[i][1]]++;
+    }
+
+    int count = 0;
+    for(int i = 0; i < n; i++) {
+        if(indegree[i] == 0) {
+            count++;
+        }
     }
 
     int *answer = (int *)malloc(count * sizeof(int));
