ebaaad1e16e9f38b1bc20c330a451f86093a33c5
Vedant Manoj Salunke <vedantsalunke1234@gmail.com>
2026-02-06 14:39:30 +0530

    Time: 0 ms (100.00%) | Memory: 16.8 MB (72.92%) - LeetSync

---
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int rows = grid.size();
+        int cols = grid[0].size();
+        int minutes = 0, fresh = 0;
+        queue<pair<int, int>> q;
+        
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < cols; j++) {
+                if(grid[i][j] == 2) {
+                    q.push({i, j});
+                }
+                else if(grid[i][j] == 1) {
+                    fresh++;
+                }
+            }
+        }
+
+        while(!q.empty() && fresh > 0) {
+            int level = q.size();
+
+            for(int i = 0; i < level; i++) {
+                int r = q.front().first;
+                int c = q.front().second;
+                q.pop();
+
+                int dr[] = {-1, 1, 0, 0};
+                int dc[] = {0, 0, -1, 1};
+
+                for(int j = 0; j < 4; j++) {
+                    int nr = r + dr[j];
+                    int nc = c + dc[j];
+
+                    if(nr < rows && nr >= 0 && nc < cols && nc >= 0&& grid[nr][nc] == 1) {
+                        grid[nr][nc] = 2;
+                        q.push({nr, nc});
+                        fresh--;
+                    }
+                }
+            }
+            minutes++;
+        }
+
+        return fresh == 0 ? minutes : -1;
+    }
+};
