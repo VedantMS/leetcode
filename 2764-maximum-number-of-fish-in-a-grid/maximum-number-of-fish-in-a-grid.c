712da939711a2eea774852e23447790e66ef3bd9
Vedant Manoj Salunke <vedantsalunke1234@gmail.com>
2026-02-22 14:50:10 +0530

    Time: 8 ms (7.14%) | Memory: 12.9 MB (35.71%) - LeetSync

--- 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.c
+++ 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.c
@@ -1,8 +1,5 @@
-int maxFish;
-int tempFish;
-
-void dfs(int **grid, int rows, int cols, int r, int c) {
-    tempFish += grid[r][c];
+int dfs(int **grid, int rows, int cols, int r, int c) {
+    int fish = grid[r][c];
 
     grid[r][c] = 0;
 
@@ -14,23 +11,23 @@ void dfs(int **grid, int rows, int cols, int r, int c) {
         int nc = c + dc[i];
 
         if(nr < rows && nr >= 0 && nc < cols && nc >= 0 && grid[nr][nc] > 0) {
-            dfs(grid, rows, cols, nr, nc);
+            fish += dfs(grid, rows, cols, nr, nc);
         }
     }
+
+    return fish;
 }
 
 int findMaxFish(int** grid, int gridSize, int* gridColSize) {
     int rows = gridSize;
     int cols = *gridColSize;
 
-    maxFish = 0;
-    tempFish = 0;
+    int maxFish = 0;
 
     for(int i = 0; i < rows; i++) {
         for(int j = 0; j < cols; j++) {
             if(grid[i][j] > 0) {
-                tempFish = 0;
-                dfs(grid, rows, cols, i, j);
+                int tempFish = dfs(grid, rows, cols, i, j);
                 if(maxFish < tempFish) {
                     maxFish = tempFish;
                 }
