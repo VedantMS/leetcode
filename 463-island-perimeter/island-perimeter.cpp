9a2d1ca2d4500249e3df699250cbd2a46693a287
Vedant Manoj Salunke <vedantsalunke1234@gmail.com>
2026-02-24 22:33:45 +0530

    Time: 7 ms (44.32%) | Memory: 102.1 MB (67.06%) - LeetSync

--- 463-island-perimeter/island-perimeter.cpp
+++ 463-island-perimeter/island-perimeter.cpp
@@ -1,43 +1,27 @@
 class Solution {
 public:
-    static constexpr int dr[] = {-1, 1, 0, 0};
-    static constexpr int dc[] = {0, 0, -1, 1};
-    int perimeter;
-
-    void dfs(vector<vector<int>> &grid, int r, int c) {
-        int rows = grid.size();
-        int cols = grid[0].size();
-        int count = 0;
-
-        grid[r][c] = 2;
-
-        for(int i = 0; i < 4; i++) {
-            int nr = r + dr[i];
-            int nc = c + dc[i];
-
-            if(nr < rows && nr >= 0 && nc < cols && nc >= 0 && grid[nr][nc] > 0) {
-                if(grid[nr][nc] == 1) {
-                    dfs(grid, nr, nc);
-                }
-
-                count++;
-            }
-        }
-
-        perimeter += 4 - count;
-    }
-
     int islandPerimeter(vector<vector<int>>& grid) {
         int rows = grid.size();
         int cols = grid[0].size();
 
-        perimeter = 0;
+        int dr[] = {-1, 1, 0, 0};
+        int dc[] = {0, 0, -1, 1};
+
+        int perimeter = 0;
 
         for(int i = 0; i < rows; i++) {
             for(int j = 0; j < cols; j++) {
                 if(grid[i][j] == 1) {
-                    dfs(grid, i, j);
-                    break;
+                    perimeter += 4;
+
+                    for(int k = 0; k < 4; k++) {
+                        int nr = i + dr[k];
+                        int nc = j + dc[k];
+                        
+                        if(nr < rows && nr >= 0 && nc < cols && nc >= 0 && grid[nr][nc]) {
+                            perimeter--;
+                        }
+                    }
                 }
             }
         }
