a1cd045394ae1c9603c3e109f07ff7639958e351
Vedant Manoj Salunke <vedantsalunke1234@gmail.com>
2026-02-24 22:36:35 +0530

    Time: 0 ms (100.00%) | Memory: 14.5 MB (65.36%) - LeetSync

--- 463-island-perimeter/island-perimeter.c
+++ 463-island-perimeter/island-perimeter.c
@@ -1,39 +1,25 @@
-static const int dr[] = {-1, 1, 0, 0};
-static const int dc[] = {0, 0, -1, 1};
-int perimeter;
-
-void dfs(int **grid, int rows, int cols, int r, int c) {
-    grid[r][c] = 2;
-
-    int count = 0;
-
-    for(int i = 0; i < 4; i++) {
-        int nr = r + dr[i];
-        int nc = c + dc[i];
-
-        if(nr < rows && nr >= 0 && nc < cols && nc >= 0 && grid[nr][nc] > 0) {
-            if(grid[nr][nc] == 1) {
-                dfs(grid, rows, cols, nr, nc);
-            }
-
-            count++;
-        }
-    }
-
-    perimeter += 4 - count;
-}
-
 int islandPerimeter(int** grid, int gridSize, int* gridColSize) {
     int rows = gridSize;
     int cols = *gridColSize;
 
-    perimeter = 0;
+    int dr[] = {-1, 1, 0, 0};
+    int dc[] = {0, 0, -1, 1};
+
+    int perimeter = 0;
 
     for(int i = 0; i < rows; i++) {
         for(int j = 0; j < cols; j++) {
             if(grid[i][j] == 1) {
-                dfs(grid, rows, cols, i, j);
-                break;
+                perimeter += 4;
+
+                for(int k = 0; k < 4; k++) {
+                    int nr = i + dr[k];
+                    int nc = j + dc[k];
+
+                    if(nr < rows && nr >= 0 && nc < cols && nc >= 0 && grid[nr][nc]) {
+                        perimeter--;
+                    }
+                }
             }
         }
     }
