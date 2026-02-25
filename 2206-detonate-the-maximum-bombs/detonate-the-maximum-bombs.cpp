37f637b7d2a437740f74988920f3777a599d0fb2
Vedant Manoj Salunke <vedantsalunke1234@gmail.com>
2026-02-25 23:36:46 +0530

    Time: 0 ms (100.00%) | Memory: 18.6 MB (94.22%) - LeetSync

---
+++ 2206-detonate-the-maximum-bombs/detonate-the-maximum-bombs.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    int dfs(vector<vector<int>>& adj, vector<bool> &visited, int node) {
+        visited[node] = true;
+        int num = 1;
+        for(int i : adj[node]) {
+            if(!visited[i]) {
+                num += dfs(adj, visited, i);
+            }
+        }
+        
+        return num;
+    }
+
+    int maximumDetonation(vector<vector<int>>& bombs) {
+        int n = bombs.size();
+        vector<vector<int>> adj(n);
+
+        for(int i = 0; i < n; i++) {
+            long long x1 = bombs[i][0];
+            long long y1 = bombs[i][1];
+            long long r = bombs[i][2];
+
+            for(int j = 0; j < n; j++) {
+                if(i == j) {
+                    continue;
+                }
+
+                long long x2 = bombs[j][0];
+                long long y2 = bombs[j][1];
+
+                if(((x1 - x2) * (x1 - x2)) + ((y1 - y2) * (y1 - y2)) <= pow(r, 2)) {
+                    adj[i].push_back(j);
+                }
+            }
+        }
+
+        int ans = 0;
+
+        for(int i = 0; i < n; i++) {
+            vector<bool> visited(n, false);
+            int num = dfs(adj, visited, i);
+            
+            if(num == n) {
+                return n;
+            }
+
+            if(ans < num) {
+                ans = num;
+            }
+        }
+
+        return ans;
+    }
+};
