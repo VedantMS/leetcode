e1f5ff99a1f2aee4fc0121c222584c10d8191202
Vedant Manoj Salunke <vedantsalunke1234@gmail.com>
2026-03-10 13:40:03 +0530

    Time: 33 ms (63.55%) | Memory: 32.1 MB (83.23%) - LeetSync

---
+++ 1397-search-suggestions-system/search-suggestions-system.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
+        sort(products.begin(), products.end());
+
+        vector<vector<string>> ans;
+        string prefix = "";
+
+        for(char c : searchWord) {
+            prefix += c;
+            vector<string> temp;
+
+            for(string &product : products) {
+                if(product.find(prefix) == 0) {
+                    temp.push_back(product);
+                    if(temp.size() == 3) {
+                        break;
+                    }
+                }
+            }
+
+            ans.push_back(temp);
+        }
+
+        return ans;
+    }
+};
