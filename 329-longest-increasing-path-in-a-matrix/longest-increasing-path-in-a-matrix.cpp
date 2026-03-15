1c145b14f7a2e8f2beafe3716590182bf730cbe2
Vedant Manoj Salunke <vedantsalunke1234@gmail.com>
2026-03-15 14:42:52 +0530

    Time: 9 ms (68.75%) | Memory: 19 MB (96.74%) - LeetSync

---
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int memo[201][201];
+
+    static constexpr int dr[] = {-1, 1, 0, 0};
+    static constexpr int dc[] = {0, 0, -1, 1};
+
+    int dfs(vector<vector<int>>& matrix, int r, int c) {
+        if(memo[r][c] != 0) {
+            return memo[r][c];
+        }
+        
+        int maxlen = 1;
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+
+        for(int i = 0; i < 4; i++) {
+            int nr = r + dr[i];
+            int nc = c + dc[i];
+
+            if(nr < rows && nr >= 0 && nc < cols && nc >= 0 && matrix[nr][nc] > matrix[r][c]) {
+                maxlen = max(maxlen, 1 + dfs(matrix, nr, nc));
+            }
+        }
+        
+        return memo[r][c] = maxlen;
+    }
+
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        int ans = 0;
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+
+        memset(memo, 0, sizeof(memo));
+
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < cols; j++) {
+                int len = dfs(matrix, i, j);
+                if(ans < len) {
+                    ans = len;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
