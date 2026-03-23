b27319e525b08971803bd239a5608cac84ce0bd5
Vedant Manoj Salunke <vedantsalunke1234@gmail.com>
2026-03-23 20:27:34 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (23.10%) - LeetSync

---
+++ 404-sum-of-left-leaves/sum-of-left-leaves.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sumOfLeftLeaves(TreeNode* root) {
+        int ans = 0;
+
+        queue<TreeNode *> q;
+        q.push(root);
+
+        while(!q.empty()) {
+            int level = q.size();
+
+            for(int i = 0; i < level; i++) {
+                auto node = q.front();
+                q.pop();
+
+                if(node->left) {
+                    if(!node->left->left && !node->left->right) {
+                        ans += node->left->val;
+                    }
+                    q.push(node->left);
+                }
+
+                if(node->right) {
+                    q.push(node->right);
+                }
+            }
+        }
+
+        return ans;
+    }
+};
