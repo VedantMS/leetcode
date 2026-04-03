7073b42630fc52f85360cad97288bea43b925d04
Vedant Manoj Salunke <vedantsalunke1234@gmail.com>
2026-04-03 13:25:36 +0530

    Time: 53 ms (86.77%) | Memory: 86.3 MB (68.87%) - LeetSync

--- 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
+++ 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
         vector<vector<int>> adj(n);
-        vector<set<int>> temp(n);
+        vector<vector<int>> ans(n);
         vector<int> indegree(n, 0);
         vector<bool> visited(n, false);
 
@@ -14,42 +14,29 @@ public:
             indegree[v]++;
         }
 
-        queue<int> q;
+        for(int i = 0; i < n; i++) {
+            vector<bool> visited(n, false);
 
-        while(1) {
-            bool push = false;
-            for(int i = 0; i < n; i++) {
-                if(indegree[i] == 0 && !visited[i]) {
-                    q.push(i);
-                    push = true;
-                }
-            }
+            queue<int> q;
+            q.push(i);
+
+            visited[i] = true;
 
             while(!q.empty()) {
                 int u = q.front();
                 q.pop();
 
-                visited[u] = true;
-
                 for(int v : adj[u]) {
-                    temp[v].insert(u);
-                    indegree[v]--;
-
-                    for(int a : temp[u]) {
-                        temp[v].insert(a);
+                    if(!visited[v]) {
+                        visited[v] = true;
+                        
+                        ans[v].push_back(i);
+                        q.push(v);
                     }
                 }
             }
-
-            if(!push)   break;
         }
-
-        vector<vector<int>> ans(n);
-
-        for(int i = 0; i < n; i++) {
-            ans[i].assign(temp[i].begin(), temp[i].end());
-        }
-
+        
         return ans;
     }
 };
