dbf4d75360ccceb3cc5018cf104ee05dfa401030
Vedant Manoj Salunke <vedantsalunke1234@gmail.com>
2026-04-14 10:27:10 +0530

    Time: 3 ms (80.89%), Space: 11.2 MB (90.97%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int index = 0;
+        int index = 0, i = 0;
         int ans = 0;
         int n = s.size();
 
-        unordered_set<char> charSet;
+        int arr[256] = {0};
 
-        for(int i = 0; i < n; i++) {
-            while(charSet.find(s[i]) != charSet.end()) {
-                charSet.erase(s[index]);
+        while(i < n) {
+            if(arr[s[i]] == 0) {
+                arr[s[i]]++;
+                i++;
+            }
+
+            else {
+                arr[s[index]]--;
                 index++;
             }
 
-            charSet.insert(s[i]);
-            ans = max(ans, i - index + 1);
+            if(i - index > ans) {
+                ans = i - index;
+            }
         }
 
         return ans;
