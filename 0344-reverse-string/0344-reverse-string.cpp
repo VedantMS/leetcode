c77220fd66ecd61a008f1d671a91687572c6bf6e
Vedant Manoj Salunke <vedantsalunke1234@gmail.com>
2026-06-29 23:39:41 +0530

    Time: 0 ms (100%), Space: 27.3 MB (15.42%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int n = s.size();
+        int low = 0, high = n - 1;
+
+        while (low < high) {
+            char ch = s[low];
+            s[low] = s[high];
+            s[high] = ch;
+
+            low++;
+            high--;
+        }
+    }
+};
