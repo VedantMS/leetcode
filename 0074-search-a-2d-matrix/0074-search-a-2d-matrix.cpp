036907ef6cfe4fc605cc2199da24ec97df205173
Vedant Manoj Salunke <vedantsalunke1234@gmail.com>
2026-07-05 20:34:19 +0530

    Time: 0 ms (100%), Space: 13.2 MB (78.94%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+
+        for (int i = 0; i < rows; i++) {
+            if (matrix[i][cols - 1] >= target) {
+                for (int j = 0; j < cols; j++) {
+                    if (matrix[i][j] == target) {
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+        }
+
+        return false;
+    }
+};
