41d6f07aa563214f398632ecff970b8cfdf52900
Vedant Manoj Salunke <vedantsalunke1234@gmail.com>
2026-07-23 18:37:50 +0530

    Time: 0 ms (100%), Space: 9.3 MB (45.66%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
-    void backtrack(string &digits, int idx, string s, vector<string> &ans, unordered_map<char, string> &digitToLetters) {
-        if(idx == digits.length()) {
+    void backtrack(string digits, int index, unordered_map<char, string> &mp, string &s, vector<string> &ans) {
+        if (index == digits.size()) {
             ans.push_back(s);
             return;
         }
 
-        string letters = digitToLetters.at(digits[idx]);
-        for(char letter : letters) {
-            backtrack(digits, idx + 1, s + letter, ans, digitToLetters);
+        for (char &ch : mp[digits[index]]) {
+            s.push_back(ch);
+
+            backtrack(digits, index + 1, mp, s, ans);
+
+            s.pop_back();
         }
     }
+
     vector<string> letterCombinations(string digits) {
+        unordered_map<char, string> mp = {{'2', "abc"}, {'3', "def"}, {'4', "ghi"}, {'5', "jkl"}, {'6', "mno"}, {'7', "pqrs"}, {'8', "tuv"}, {'9', "wxyz"}};
         vector<string> ans;
 
-        unordered_map<char, string> digitToLetters = {
-            {'2', "abc"},
-            {'3', "def"},
-            {'4', "ghi"},
-            {'5', "jkl"},
-            {'6', "mno"},
-            {'7', "pqrs"},
-            {'8', "tuv"},
-            {'9', "wxyz"}
-        };
+        string s = "";
 
-        backtrack(digits, 0, "", ans, digitToLetters);
+        backtrack(digits, 0, mp, s, ans);
 
         return ans;
     }
