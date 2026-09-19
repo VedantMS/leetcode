0adb260baf79b80fae8c71ea62bceb88cf3d105a
Vedant Manoj Salunke <vedantsalunke1234@gmail.com>
2026-09-19 20:12:48 +0530

    Time: 8 ms (99.9%), Space: 14.4 MB (95.35%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,41 +1,20 @@
 class Solution {
 public:
     int minCostConnectPoints(vector<vector<int>>& points) {
-        int n = points.size();
-        vector<vector<pair<int, int>>> adj(n);
+        int n = points.size(), ans = 0;
+        vector<int> dist(n, INT_MAX);
 
         for (int i = 0; i < n - 1; i++) {
             for (int j = i + 1; j < n; j++) {
-                int dist = abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1]);
-                adj[i].push_back({j, dist});
-                adj[j].push_back({i, dist});
-            }
-        }
-
-        int ans = 0;
-        
-        vector<bool> visited(n, false);
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-
-        //  dist, vertex
-        pq.push({0, 0});
-
-        while (!pq.empty()) {
-            auto [dist, u] = pq.top();
-            pq.pop();
-
-            if (visited[u]) {
-                continue;
-            }
-
-            visited[u] = true;
-            ans += dist;
-
-            for (auto &[v, d] : adj[u]) {
-                if (!visited[v]) {
-                    pq.push({d, v});
+                dist[j] = min(dist[j], abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1]));
+                
+                if (dist[j] < dist[i + 1]) {
+                    swap(points[j], points[i + 1]);
+                    swap(dist[j], dist[i + 1]);
                 }
             }
+
+            ans += dist[i + 1];
         }
 
         return ans;
