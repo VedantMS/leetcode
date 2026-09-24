6d1b441ea47a80d49c9a91088308d091fce4349f
Vedant Manoj Salunke <vedantsalunke1234@gmail.com>
2026-09-24 13:06:25 +0530

    Time: 0 ms (100%), Space: 30.9 MB (81.66%) - LeetHub

---
+++ 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int smallestIndex(vector<int>& nums) {
+        for (int i = 0; i < nums.size(); i++) {
+            int num = 0;
+
+            while (nums[i]) {
+                num += nums[i] % 10;
+                nums[i] /= 10;
+            }
+
+            if (num == i) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
